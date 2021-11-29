84d62b605409588c798ccc36470d14b268e95ce7
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-11-29 20:51:39 -0300

    adicionei key no map

--- LabCommerce/src/components/Cards.js
+++ LabCommerce/src/components/Cards.js
@@ -1,53 +1,50 @@
-import React from "react";
-import styled from "styled-components"
-import Produtos from "./Produtos"
+import React from 'react';
+import styled from 'styled-components';
+import Produtos from './Produtos';
 
 const Nav = styled.div`
-display: flex;
-justify-content: space-around;
-margin-bottom: 20px;
-padding: 10px;`
-
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 20px;
+  padding: 10px;
+`;
 
 const ListaProdutos = styled.div`
-display: grid;
-grid-template-columns: 1fr 1fr 1fr;
-grid-template-rows: 1fr 1fr 1fr;
-grid-row-gap: 30px;
-padding: 5px;
-margin: 10px;
-`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-row-gap: 30px;
+  padding: 5px;
+  margin: 10px;
+`;
 
 const Container = styled.div`
-border: solid 1px black;
-margin: 10px;
-padding: 10px;
-`
+  border: solid 1px black;
+  margin: 10px;
+  padding: 10px;
+`;
 
 class Cards extends React.Component {
-    render() {
-        return (
-            <Container>
-                <Nav>
-                    <p>Produtos disponiveis:</p>
-                    <div>
-                        <label>Ordenar por ordem:</label>
-                        <select>
-                            <option>Crescente</option>
-                            <option>Decrescente</option>
-                        </select>
-                    </div>
-                </Nav>
-                <ListaProdutos>
-                    <Produtos />
-                    <Produtos />
-                    <Produtos />
-                    <Produtos />
-                </ListaProdutos>
-
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Nav>
+          <p>Produtos disponiveis:</p>
+          <div>
+            <label>Ordenar por ordem:</label>
+            <select>
+              <option>Crescente</option>
+              <option>Decrescente</option>
+            </select>
+          </div>
+        </Nav>
+        <ListaProdutos>
+          {this.props.produtos.map((produto) => (
+            <Produtos key={produto.id} produtos={produto} />
+          ))}
+        </ListaProdutos>
+      </Container>
+    );
+  }
 }
 
 export default Cards;
