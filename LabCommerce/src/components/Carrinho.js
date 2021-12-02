aa2178267fa8d793a9d523a5f4762f6e791edeae
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-12-01 21:59:13 -0300

    funcao add carrinho refatorada

--- LabCommerce/src/components/Carrinho.js
+++ LabCommerce/src/components/Carrinho.js
@@ -20,7 +20,7 @@ class Carrinho extends React.Component {
         <h4>Carrinho</h4>
         {this.props.produtosCarrinho.map((produto) => (
           <ContainerFlex>
-            <p>{produto.quantidade}</p>
+            <p>{produto.quantidade}x</p>
             <p>{produto.nome}</p>
             <p>R$ {produto.preco},00</p>
           </ContainerFlex>
